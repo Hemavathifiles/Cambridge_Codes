883c734016c3990114bb59e8ebf0fa30cb87cd64
Admin <Admin@LAPTOP-C4A1OM7H>
2026-08-21 18:26:56 +0530

    first commit

--- Cambridge/src/org/tnsif/acc/c2tc/collectionsdemo/ComparableDemo.java
+++ Cambridge/src/org/tnsif/acc/c2tc/collectionsdemo/ComparableDemo.java
@@ -3,45 +3,51 @@ package org.tnsif.acc.c2tc.collectionsdemo;
 import java.util.ArrayList;
 import java.util.Collections;
 
-class Person6 implements Comparable<Person6>
+class Person5 implements Comparable<Person5>
 {
-      String name;
-      int age;
-      
-      Person6(String name,int age)
-      {
-    	      this.name=name;
-    	      this.age=age;
-      }
-	  void display()
-	  {
-		  System.out.println("Name :"+name +"  Age :"+age);
-	  }
+
+	String name;
+	int age;
 	
+	public Person5(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+     void display()
+     {
+    	 System.out.println("Name :"+name + " , Age :"+age);
+     }
 	
 	@Override
-	public int compareTo(Person6 other) {
-		
-		return this.name.compareTo(other.name);
+	public int compareTo(Person5 o) {
+	
+		return this.name.compareTo(o.name);
 	}
 	
+	
+	
 }
-
 
 public class ComparableDemo {
 
 	public static void main(String[] args) {
-		ArrayList<Person6> people=new ArrayList<>();
-		people.add(new Person6("John",30));
-		people.add(new Person6("Anil",20));
-		people.add(new Person6("Baskar",17));
+		ArrayList<Person5> people =new ArrayList<>();
+		
+		people.add(new Person5("John",27));
+		people.add(new Person5 ("Anil",30));
+		people.add(new Person5 ("Baskar",17));
+		
+		System.out.println("Sort by Name");
 		
 		Collections.sort(people);
-
-		for(Person6 person:people)
+		
+		for(Person5 person:people)
 		{
 			person.display();
 		}
+		
 	}
 
 }
